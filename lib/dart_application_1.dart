b60ba4033dbbf87d6f166124e34a77082daed362
Darrin-Babs <darrinkingbest01@gmail.com>
2025-04-18 16:47:33 -0400

    Add Person class and update main function

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,8 +1,25 @@
-enum Spin {up, down,}
+enum Spin { up, down }
 
 class Electron {
- int? id;
-  static const num  = 9.109e-28;
-  Spin _spin;
+  int? id;
+  static const num mass = 9.109e-28;
+  final Spin _spin = Spin.down;
   Electron? _entangledElectron;
+}
+
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+
+  void displayInfo() {
+    print('Name: $name');
+    print('Age: $age');
+  }
+}
+
+void main() {
+  Person person = Person('Alice', 30);
+  person.displayInfo();
 }
